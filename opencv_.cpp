f689ee719fe537914ce82883647521e738cd07e1
Tsuchida <hiromichi.tsuchida@jp.showa1.com>
2021-08-26 13:14:13 +0900

    テンプレを修正した

--- opencv_.cpp
+++ opencv_.cpp
@@ -5,13 +5,17 @@
 using namespace cv;
 int main(){
     std::string win_src = "src";
+    std::string win_gray = "gray";
     std::string win_dst = "dst";
     std::string file_src = "src.png";
     std::string file_dst = "dst.png";
     Mat img_src =  imread( file_src, 1 );
+    Mat img_gray;
+    cvtColor( img_src, img_gray, COLOR_BGR2GRAY );
     Mat img_dst;
 
     namedWindow( win_src, WINDOW_AUTOSIZE );
+    namedWindow( win_gray, WINDOW_AUTOSIZE );
     namedWindow( win_dst, WINDOW_AUTOSIZE );
 
     //ここに書く
@@ -19,6 +23,7 @@ int main(){
     ////////////
     
     imshow( win_src, img_src );
+    imshow( win_gray, img_gray );
     imshow( win_dst, img_dst );
     imwrite( file_dst, img_dst );
 
