b85443d2c0618439b3d7fb0c42655037ef51229f
Tsuchida <hiromichi.tsuchida@jp.showa1.com>
2021-08-27 18:04:37 +0900

    テンプレ修正

--- opencv_.cpp
+++ opencv_.cpp
@@ -12,11 +12,10 @@ int main(){
     std::string file_dst = "dst.png";
     Mat img_src =  imread( file_src, 1 );
     Mat img_gray;
-    cvtColor( img_src, img_gray, COLOR_BGR2GRAY );
+    cvtColor( img_src, img_src, COLOR_BGR2GRAY );
     Mat img_dst;
 
     namedWindow( win_src, WINDOW_AUTOSIZE );
-    namedWindow( win_gray, WINDOW_AUTOSIZE );
     namedWindow( win_dst, WINDOW_AUTOSIZE );
 
     //ここに書く
@@ -24,9 +23,7 @@ int main(){
     ////////////
     
     imshow( win_src, img_src );
-    imshow( win_gray, img_gray );
     imshow( win_dst, img_dst );
-    imwrite( file_gray, img_gray );
     imwrite( file_dst, img_dst );
 
     waitKey(0);
