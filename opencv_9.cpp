d431dfba2b6edefdd212431d5d863f992de954d2
Tsuchida <hiromichi.tsuchida@jp.showa1.com>
2021-08-27 12:26:01 +0900

    中央値フィルタ kansei

--- opencv_9.cpp
+++ opencv_9.cpp
@@ -30,12 +30,37 @@ int main(){
     for( int y = 0; y < height_spiked; y++ ){
         for( int x = 0; x < width_spiked; x++ ){
             if( ( y*step_spiked + x*channels_spiked )%500 == 0 ){
-                img_spiked.data[ y*step_spiked + x*channels_spiked ] = 0;
+                img_gray.data[ y*step_spiked + x*channels_spiked ] = 0;
             }
         }
     }
+    img_dst = img_gray.clone();
+    int values[9];
+    for( int y = 0; y < img_gray.rows; y++){
+        for( int x = 0; x < img_gray.cols; x++ ){
+            int k = 0;
+            for( int i = -1; i <= 1; i++ ){
+                if( y + i < 0 ) continue;
+                if( y + i >= img_gray.rows ) continue;
+                for( int j = -1; j <= 1; j++ ){
+                    if( x + j < 0 ) continue;
+                    if( x + j >= img_gray.cols ) continue;
+                    values[k++] =  img_gray.data[ (y+i)*img_gray.step + (x+j)*img_gray.channels() ];
+                }
+            }
+            for( int i = 0; i < 9; i++ ){
+                for( int j = 8; j > i; j-- ){
+                    if( values[j-1] > values[j] ){
+                        int tmp = values[j];
+                        values[j] = values[j-1];
+                        values[j-1] = tmp;
+                    }
+                }
+            }
+            img_dst.data[ y*img_dst.step + x*img_dst.channels() ] = values[4];
+        }
+    }
 
-    img_dst = img_spiked.clone();
     ////////////
     
     imshow( win_src, img_src );
