d0541b81aec330388f2e24f5b497de7a94a16d22
Tsuchida <hiromichi.tsuchida@jp.showa1.com>
2021-09-01 17:46:14 +0900

    Canny Edge Detection

---
+++ opencv_012.cpp
@@ -0,0 +1,31 @@
+//
+#include <iostream>
+#include <cmath>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+int main(){
+    std::string win_src = "src";
+    std::string win_gray = "gray";
+    std::string win_dst = "dst";
+    std::string file_src = "src.png";
+    std::string file_gray = "gray.png";
+    std::string file_dst = "dst.png";
+    Mat img_src =  imread( file_src, 1 );
+    Mat img_gray;
+    cvtColor( img_src, img_src, COLOR_BGR2GRAY );
+    Mat img_dst;
+
+    namedWindow( win_src, WINDOW_AUTOSIZE );
+    namedWindow( win_dst, WINDOW_AUTOSIZE );
+
+    //ここに書く
+    Canny(img_src, img_dst, 155, 255 );
+    ////////////
+    
+    imshow( win_src, img_src );
+    imshow( win_dst, img_dst );
+    imwrite( file_dst, img_dst );
+
+    waitKey(0);
+    return 0;
+}                                                                                                                                                                                                        
